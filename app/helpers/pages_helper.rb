d72c7942f4dc7e8a3dbac3cfe03cdd206ee20dbe
Pekka Korhonen <p.korhonen@elisanet.fi>
2017-10-14 22:33:50 +0300

    Built twitter parser for tweet links

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,28 @@
 module PagesHelper
+  def twitter_parser tweet
+    regex = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+
+    tweet.gsub(regex) do |url|
+      "<a href='#{url}' target='_blank'>#{url}</a>"
+    end.html_safe
+  end
 end
+
+"RoRNewsandNotes: RT @_rails: Add accept-charset to the output of form_with in JS guide [ci skip]  <a href=https://t.co/bELCDIdK1b target='_blank'>https://t.co/bELCDIdK1b</a>. #rails #rubyonrails"
+
